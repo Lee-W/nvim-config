2003311cee8bc71b6270ebf8a134390716ebd887
Wei Lee <weilee.rx@gmail.com>
2023-10-16 17:46:14 +0800

    feat: comment out all the content in disabled.lua
    it might accidentally ignore plugins used by other plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,22 +1,22 @@
 return {
   ---- utils
-  {
-    -- not used
-
-    "nvim-pack/nvim-spectre",
-    -- nnoremap <leader>S :lua require('spectre').open()<CR>
-    -- "search current word
-    -- nnoremap <leader>sw :lua require('spectre').open_visual({select_word=true})<CR>
-    -- vnoremap <leader>s :lua require('spectre').open_visual()<CR>
-    -- "  search in current file
-    -- nnoremap <leader>sp viw:lua require('spectre').open_file_search()<cr>
-    enabled = false,
-  },
-  {
-    -- replaced by lazyvim setup
-    "glepnir/dashboard-nvim",
-    enabled = false,
-  },
+  -- {
+  --   -- not used
+  --
+  --   "nvim-pack/nvim-spectre",
+  --   -- nnoremap <leader>S :lua require('spectre').open()<CR>
+  --   -- "search current word
+  --   -- nnoremap <leader>sw :lua require('spectre').open_visual({select_word=true})<CR>
+  --   -- vnoremap <leader>s :lua require('spectre').open_visual()<CR>
+  --   -- "  search in current file
+  --   -- nnoremap <leader>sp viw:lua require('spectre').open_file_search()<cr>
+  --   enabled = false,
+  -- },
+  -- {
+  --   -- replaced by lazyvim setup
+  --   "nvimdev/dashboard-nvim",
+  --   enabled = false,
+  -- },
   -- {
   -- cannot setup correctly as of now, but lazyvim's default setting works
   -- "junegunn/fzf",
@@ -44,12 +44,12 @@ return {
   -- },
 
   ---- snippets
-  {
-    "SirVer/ultisnips",
-    enabled = false,
-  },
-  {
-    "honza/vim-snippets",
-    enabled = false,
-  },
+  -- {
+  --   "SirVer/ultisnips",
+  --   enabled = false,
+  -- },
+  -- {
+  --   "honza/vim-snippets",
+  --   enabled = false,
+  -- },
 }
