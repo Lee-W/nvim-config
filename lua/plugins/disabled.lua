b7aeb3ac3d354b24fb2564325e6d475ebdf1009f
Wei Lee <weilee.rx@gmail.com>
2024-04-22 19:49:05 +0800

    refactor: clean up unused plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,35 +1,17 @@
 return {
   ---- utils
-  -- {
-  --   -- not used
-  --
-  --   "nvim-pack/nvim-spectre",
-  --   -- nnoremap <leader>S :lua require('spectre').open()<CR>
-  --   -- "search current word
-  --   -- nnoremap <leader>sw :lua require('spectre').open_visual({select_word=true})<CR>
-  --   -- vnoremap <leader>s :lua require('spectre').open_visual()<CR>
-  --   -- "  search in current file
-  --   -- nnoremap <leader>sp viw:lua require('spectre').open_file_search()<cr>
-  --   enabled = false,
-  -- },
-  -- {
-  -- cannot setup correctly as of now, but lazyvim's default setting works
-  -- "junegunn/fzf",
-  -- enabled = false,
-  -- },
-  -- {
-  -- cannot setup correctly as of now, but lazyvim's default setting works
-  -- "junegunn/fzf.vim",
-  -- opts = {
-  --   wildignore = { "*/tmp/*", "*.so", "*.swp", "*.zip" },
-  --   fzf_buffer_jump = 1,
-  -- },
-  -- keys = {
-  --   { "<C-F>", "<cmd>Rg<cr>" },
-  --   { ";", ":Buffers<cr>" },
-  -- },
-  -- enabled = false,
-  -- },
+  {
+    -- not used
+
+    "nvim-pack/nvim-spectre",
+    -- nnoremap <leader>S :lua require('spectre').open()<CR>
+    -- "search current word
+    -- nnoremap <leader>sw :lua require('spectre').open_visual({select_word=true})<CR>
+    -- vnoremap <leader>s :lua require('spectre').open_visual()<CR>
+    -- "  search in current file
+    -- nnoremap <leader>sp viw:lua require('spectre').open_file_search()<cr>
+    enabled = false,
+  },
 
   ---- snippets
   -- {
@@ -40,4 +22,19 @@ return {
   --   "honza/vim-snippets",
   --   enabled = false,
   -- },
+  ---- git
+  {
+
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua", -- optional
+    },
+    config = true,
+    enabled = false,
+  },
 }
