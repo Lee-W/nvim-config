5e99575cf1f36cc8213f35979a2cf70e580cc1ae
Wei Lee <weilee.rx@gmail.com>
2024-04-23 16:59:53 +0800

    feat: update nvim-tree.lua config

--- lua/plugins/file_management.lua
+++ lua/plugins/file_management.lua
@@ -1,39 +1,31 @@
-local function getTelescopeOpts(state, path)
-  return {
-    cwd = path,
-    search_dirs = { path },
-    attach_mappings = function(prompt_bufnr, map)
-      local actions = require("telescope.actions")
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local action_state = require("telescope.actions.state")
-        local selection = action_state.get_selected_entry()
-        local filename = selection.filename
-        if filename == nil then
-          filename = selection[1]
-        end
-        -- any way to open the file without triggering auto-close event of neo-tree?
-        require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
-      end)
-      return true
-    end,
-  }
-end
-
 return {
   {
     "kyazdani42/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    lazy = false,
     config = function()
       require("nvim-tree").setup({
+        renderer = {
+          show = {
+            folder = true,
+          },
+        },
+        indent_markers = {
+          enable = false,
+        },
         filters = {
           custom = {
             "^\\.git$",
             ".mypy_cache",
             ".pytest_cache",
+            ".ruff_cache",
             ".ropeproject",
             "__pycache__",
             ".ipynb_checkpoints",
             ".hypothesis",
+            ".DS_Store",
           },
         },
         git = { ignore = false },
