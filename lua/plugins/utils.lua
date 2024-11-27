1752862246ecb662367d016a7cc8fdbd64037b60
Wei Lee <weilee.rx@gmail.com>
2024-11-27 19:14:18 +0800

    style: remove unused wilder.nvim

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -62,25 +62,6 @@ return {
   {
     "yamatsum/nvim-cursorline",
   },
-  -- {
-  --   "gelguy/wilder.nvim",
-  --   build = "UpdateRemotePlugins",
-  --   config = function()
-  --     local wilder = require("wilder")
-  --     wilder.setup({ modes = { ":", "/", "?" } })
-  --
-  --     wilder.set_option("pipeline", {
-  --       wilder.branch(wilder.cmdline_pipeline(), wilder.search_pipeline()),
-  --     })
-  --
-  --     wilder.set_option(
-  --       "renderer",
-  --       wilder.wildmenu_renderer({
-  --         highlighter = wilder.basic_highlighter(),
-  --       })
-  --     )
-  --   end,
-  -- },
   {
     "windwp/nvim-autopairs",
     config = function()
