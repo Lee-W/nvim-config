0438c3c354efcb82e8939bf1a4bdc48537a20b35
Wei Lee <weilee.rx@gmail.com>
2025-04-10 19:10:45 +0800

    docs(git): add comments to each tool

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,17 +1,22 @@
 return {
   {
-    "airblade/vim-gitgutter",
-  },
-  {
+    -- git wrapper
     "tpope/vim-fugitive",
   },
   {
+    -- conflict resolver
     "rhysd/conflict-marker.vim",
   },
   {
+    -- git diff markers
+    "airblade/vim-gitgutter",
+  },
+  {
+    -- :DiffView.*
     "sindrets/diffview.nvim",
   },
   {
+    -- Interactive git interface
     "NeogitOrg/neogit",
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
