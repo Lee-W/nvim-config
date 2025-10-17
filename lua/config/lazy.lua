717abfd8e3fc6e00f45fc641f9f8810abb319100
Wei Lee <weilee.rx@gmail.com>
2025-10-17 14:39:11 +0800

    feat: enable typescript

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.toml" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     -- lsp
     { import = "lazyvim.plugins.extras.lsp.neoconf" },
     -- editor
