04c85903c342b70926bbdeaa53c02a8a8de326e8
Wei Lee <weilee.rx@gmail.com>
2025-11-03 15:42:16 +0800

    feat: add sql as extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,15 +14,15 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     -- lang
     { import = "lazyvim.plugins.extras.lang.python" },
-    -- { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.toml" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
     -- lsp
     { import = "lazyvim.plugins.extras.lsp.neoconf" },
     -- editor
