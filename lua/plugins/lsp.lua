2e5eb89562c8068649c32ea5392241bffb85feca
Wei Lee <weilee.rx@gmail.com>
2026-03-10 09:37:24 +0800

    feat(lsp): add rumdl as markdown formatter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -92,6 +92,7 @@ return {
         python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
         rust = { "rustfmt", lsp_format = "fallback" },
         -- toml = { "taplo" },
+        markdown = { "rumdl" },
         javascript = { "prettierd", "prettier", stop_after_first = true },
         ["*"] = { "codespell" },
         ["_"] = { "trim_whitespace", "trim_newlines" },
