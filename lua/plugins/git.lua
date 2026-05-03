9661ebc2e6d45e17f30cf828dacfa871aa9dfe82
Wei Lee <weilee.rx@gmail.com>
2026-05-03 22:17:31 +0800

    feat: add FabijanZulj/blame.nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -21,4 +21,19 @@ return {
     },
     config = true,
   },
+  {
+    -- modern blame view (window + virtual modes)
+    "FabijanZulj/blame.nvim",
+    cmd = { "BlameToggle" },
+    keys = {
+      { "<leader>gB", "<cmd>BlameToggle window<cr>", desc = "Blame Buffer (window)" },
+      { "<leader>gV", "<cmd>BlameToggle virtual<cr>", desc = "Blame Buffer (virtual)" },
+    },
+    opts = {
+      date_format = "%Y-%m-%d",
+      merge_consecutive = false,
+      max_summary_width = 30,
+      commit_detail_view = "split",
+    },
+  },
 }
